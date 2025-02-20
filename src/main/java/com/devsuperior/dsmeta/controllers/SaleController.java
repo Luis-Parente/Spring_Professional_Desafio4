8f1da49d183a82726113e4ee27fc2faeb1b10093
Luis Parente <lgsp25@hotmail.com>
2025-02-19 21:12:38 -0300

    Função searchToReport funcionando

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dsmeta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -16,7 +20,7 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
 		SaleMinDTO dto = service.findById(id);
@@ -24,9 +28,11 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getReport(@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate, @RequestParam(defaultValue = "") String name,
+			@PageableDefault(size = 40) Pageable pageable) {
+		Page<SaleMinDTO> result = service.searchToReport(minDate, maxDate, name, pageable);
+		return ResponseEntity.ok(result);
 	}
 
 	@GetMapping(value = "/summary")
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -14,10 +19,30 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleMinDTO> searchToReport(String min, String max, String name, Pageable pageable) {
+		LocalDate minDate = null;
+		LocalDate maxDate = null;
+
+		if (min.isEmpty()) {
+			minDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()).minusYears(1);
+		} else {
+			minDate = LocalDate.parse(min);
+		}
+		if (max.isEmpty()) {
+			maxDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		} else {
+			maxDate = LocalDate.parse(max);
+		}
+
+		Page<SaleMinDTO> result = repository.searchToReport(minDate, maxDate, name, pageable);
+
+		return result;
 	}
 }
